7d58e73bc6cfe57c82cb472be8f3711485c8b988
Christoffer G. Thomsen <chris@cgt.name>
2020-04-12 15:10:19 +0200

    working

--- spec/potter_spec.rb
+++ spec/potter_spec.rb
@@ -1,3 +1,15 @@
 # frozen_string_literal: true
 RSpec.describe 'Sale' do
+  context 'with no items' do
+    it 'has total 0' do
+      emptySale = Sale.new
+      expect(emptySale.total).to eq 0
+    end
+  end
+end
+
+class Sale
+  def total
+    0
+  end
 end
