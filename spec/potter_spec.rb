df44f42ad3125227b70becfadafe52c0bdc2670c
Christoffer G. Thomsen <chris@cgt.name>
2020-04-12 15:34:50 +0200

    working

--- spec/potter_spec.rb
+++ spec/potter_spec.rb
@@ -2,8 +2,8 @@
 RSpec.describe 'Sale' do
   context 'with no items' do
     it 'has total 0' do
-      empty_sale = Sale.new
-      expect(empty_sale.total).to eq 0
+      sale = Sale.new
+      expect(sale.total).to eq 0
     end
   end
 
