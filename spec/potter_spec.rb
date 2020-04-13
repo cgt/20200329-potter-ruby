0486959c0c1e314a43a07e1ebe143f2e030410a0
Christoffer G. Thomsen <chris@cgt.name>
2020-04-13 13:23:26 +0200

    working

--- spec/potter_spec.rb
+++ spec/potter_spec.rb
@@ -80,6 +80,7 @@ RSpec.describe "Sale" do
     it "balance sets" do
       sale.add :first_book, :second_book, :third_book, :fourth_book
       sale.add :first_book, :second_book, :third_book, :fifth_book
+
       sets_by_size = sale.items_as_sets.group_by { |set| set.size }
       expect(sets_by_size).to eq({
         5 => [[:first_book, :second_book, :third_book, :fourth_book, :fifth_book]],
