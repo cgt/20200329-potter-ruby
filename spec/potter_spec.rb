293b45db2dabb0ffa81e55bcde14a7a3712ba40f
Christoffer G. Thomsen <chris@cgt.name>
2020-04-13 13:21:25 +0200

    working

--- spec/potter_spec.rb
+++ spec/potter_spec.rb
@@ -120,7 +120,11 @@ class Sale
 
   def items_as_sets_without_nil
     return [] if @items.empty?
-    items_as_sets.map(&method(:without_nil_elements))
+    sets = @items
+      .group_by(&:itself)
+      .values
+    items_as_sets_value = sets[0].zip(*sets[1..-1])
+    items_as_sets_value.map(&method(:without_nil_elements))
   end
 
   def items_as_sets
